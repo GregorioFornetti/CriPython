e659a7461f4c4896981f0815195a43c7b3217f4b
Gregório Fornetti Azevedo <greg.fornetti@users.noreply.github.com>
2020-05-23 00:29:44 -0300

    Testes cesar.

--- testes_simples.py
+++ testes_simples.py
@@ -12,18 +12,25 @@ def testa_cesar():  # Função que testa a cifra de césar.
     erros = 0
     separacoes('Cifra de César')
     print('Teste 1:')
-    print('Chave = 1')
-    mensagem_esperada = 'abcdefghijklmnopqrstuvwxyz'
-    print('Mensagem = abcdefghijklmnopqrstuvwxyz')
-    mensagem_esperada = 'bcdefghijklmnopqrstuvwxyza'
-    print(f'Mensagem encriptada esperada: {mensagem_esperada}')
-    mensagem_recebida = cesar.cifra_de_cesar('1', 'abcdefghijklmnopqrstuvwxyz')
-    print(f'Mensagem encriptada recebida: {mensagem_recebida}')
-    if mensagem_recebida == mensagem_esperada:
-        print('Passou!')
-    else:
-        erros += 1
-        print('Não passou!')
+    erros += imprimir_testes('1',
+                             'abcdefghijklmnopqrstuvwxyz',
+                             'bcdefghijklmnopqrstuvwxyza',
+                              cesar.cifra_de_cesar('1', 'abcdefghijklmnopqrstuvwxyz'))
+    print('Teste 2:')
+    erros += imprimir_testes('5',
+                             'abcdefghijklmnopqrstuvwxyz',
+                             'fghijklmnopqrstuvwxyzabcde',
+                              cesar.cifra_de_cesar('5', 'abcdefghijklmnopqrstuvwxyz'))
+    print('Teste 3:')
+    erros += imprimir_testes('7',
+                             'Bom dia, Boa tarde, Boa noite!',
+                             'Ivt kph, Ivh ahykl, Ivh uvpal!',
+                             cesar.cifra_de_cesar('7', 'Bom dia, Boa tarde, Boa noite!'))
+    print('Teste 4:')
+    erros += imprimir_testes('7 (tradução)',
+                             'Ivt kph, Ivh ahykl, Ivh uvpal!',
+                             'Bom dia, Boa tarde, Boa noite!',
+                             cesar.traduz_cesar('7', 'Ivt kph, Ivh ahykl, Ivh uvpal!'))
 
 
 def separacoes(cifra):  # Cria uma separação no output gerado.
@@ -34,3 +41,15 @@ def separacoes(cifra):  # Cria uma separação no output gerado.
     print('----' * 15)
     print()
 
+
+def imprimir_testes(chave, mensagem, mensagem_esperada, mensagem_recebida):  # Impressões automaticas.
+    print(f'chave = {chave}')
+    print(f'Mensagem = {mensagem}')
+    print(f'Mensagem esperada: {mensagem_esperada}')
+    print(f'Mensagem recebida: {mensagem_recebida}')
+    if mensagem_esperada == mensagem_recebida:
+        print('Passou!')
+        return 0
+    else:
+        print('Não passou!')
+        return 1

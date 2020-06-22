242db09763b42c36fb3be78312e7d9e423f0d911
GregorioFornetti <fornetti.greg@gmail.com>
2020-06-21 23:03:02 -0300

    Varias mudanças na main/ cesar

--- Cifras/cifra_de_cesar.py
+++ Cifras/cifra_de_cesar.py
@@ -1,58 +1,59 @@
 import valores
 
 
-def testa_chave_cesar(chave):  # Função validadora da chave.
-    if chave.isnumeric():  # Se a chave for numérica(ou seja, ser um numero inteiro positivo) ela é valida.
+def retorna_chave_se_for_valida(chave):  # Função validadora da chave.
+    if chave.isnumeric():
         return chave
     return False
 
-def executar_modo_apenas_letras(chave, mensagem, modo_traducao=False):  # Função que traduz e encripta com cifra de cesar.
-    if modo_traducao:
-        chave = traduz_cesar(chave)
-    else:
-        chave = testa_chave_cesar(chave)
+
+def encriptar_modo_apenas_letras(chave, mensagem):
+    chave = retorna_chave_se_for_valida(chave)
+    return mensagem_nova_modo_apenas_letras(chave, mensagem)
+
+
+def traduzir_modo_apenas_letras(chave, mensagem):
+    chave = adaptar_chave_para_traduçao_apenas_letras(chave)
+    return mensagem_nova_modo_apenas_letras(chave, mensagem)
+
+
+def mensagem_nova_modo_apenas_letras(chave, mensagem):
     if not mensagem:
-        return 'Mensagem invalida !'
+        return 'Mensagem inválida !'
     if chave:
-        mensagem_nova = ''
-        chave = int(chave) % valores.TAMANHO_ALFABETO  # Diminuir o valor da chave para o número de letras existentes no alfabeto.
-        for letra in mensagem:
-            ascii_letra = ord(letra)
-            ascii_soma = ascii_letra + chave
-            if ascii_letra >= valores.MIN_MINUSCULA and ascii_letra <= valores.MAX_MINUSCULA:  # Criptografia para letras minusculas.
-                if ascii_soma > valores.MAX_MINUSCULA:
-                    ascii_soma = 96 + (ascii_soma - valores.MAX_MINUSCULA)  # O codigo ascii passou de 'z', então deve voltar ao inicio.
-                mensagem_nova += chr(ascii_soma)
-                continue
-            if ascii_letra >= valores.MIN_MAIUSCULA and ascii_letra <= valores.MAX_MAIUSCULA:  # Criptografia para letras maiusculas.
-                if ascii_soma > valores.MAX_MAIUSCULA:
-                    ascii_soma = 64 + (ascii_soma - valores.MAX_MAIUSCULA)  # O codigo ascii passou de 'Z', então deve voltar ao inicio.
-                mensagem_nova += chr(ascii_soma)
-                continue
-            # Se chegou até aqui, quer dizer que não é letra, então simplesmente adicionar esse caracter.
-            mensagem_nova += letra
-        return mensagem_nova
+        return cesar_troca_apenas_letras(chave, mensagem)
     else:
         return 'Chave Invalida !'
 
 
-def traduz_cesar(chave, todos_caracteres=False):
-    '''
-    Função que adaptará a chave para o seu modo de tradução.
-    Recebe como parâmetro uma chave, que será adaptada para a tradução.
-    Recebe um parâmetro opcional, referente a que modo a chave deve ser adaptada (apenas letras ou vários caractéres).
-    '''
-    if testa_chave_cesar(chave):
-        if todos_caracteres:  # Adaptar a chave para a tradução do modo todos caracteres.
-            chave = valores.TAMANHO_ASCII - (int(chave) % valores.TAMANHO_ASCII)
-        else:  # Adaptar a chave para a tradução do modo simples (apenas letras).
-            chave = valores.TAMANHO_ALFABETO - (int(chave) % valores.TAMANHO_ALFABETO)
-        return chave
-    else:
-        return False
+def adaptar_chave_para_traduçao_apenas_letras(chave):  # Adaptar a chave para a tradução, caso ela seja válida.
+    chave = retorna_chave_se_for_valida(chave)
+    if chave:
+        chave = valores.TAMANHO_ALFABETO - (int(chave) % valores.TAMANHO_ALFABETO)
+    return chave
 
 
-def executar_modo_varios_caracteres(chave, mensagem, modo_traducao=False):
+def cesar_troca_apenas_letras(chave, mensagem):
+    mensagem_nova = ''
+    chave = int(chave) % valores.TAMANHO_ALFABETO  # Diminuir o valor da chave para o número de letras existentes no alfabeto.
+    for letra in mensagem:
+        ascii_letra = ord(letra)
+        ascii_soma = ascii_letra + chave
+        if letra.islower():
+            if ascii_soma > valores.MAX_MINUSCULA:
+                ascii_soma = 96 + (ascii_soma - valores.MAX_MINUSCULA)  # O codigo ascii passou de 'z', então deve voltar ao inicio.
+            mensagem_nova += chr(ascii_soma)
+            continue
+        if letra.isupper():  # Criptografia para letras maiusculas.
+            if ascii_soma > valores.MAX_MAIUSCULA:
+                ascii_soma = 64 + (ascii_soma - valores.MAX_MAIUSCULA)  # O codigo ascii passou de 'Z', então deve voltar ao inicio.
+            mensagem_nova += chr(ascii_soma)
+            continue
+        mensagem_nova += letra  # Caso a mensagem tenha caracteres especiais, eles serão adicionados sem mudanças.
+    return mensagem_nova
+
+
+def encriptar_modo_varios_caracteres(chave, mensagem):
     '''
     Função que encripta a cifra de césar no modo vários caracteres.
     Recebe como chave um número inteiro não negativo.
@@ -61,12 +62,7 @@ def executar_modo_varios_caracteres(chave, mensagem, modo_traducao=False):
     '''
     if not mensagem:
         return 'Mensagem inválida !'
-    if modo_traducao:  # Se for modo tradução, será encaminhado para outra função.
-        chave_antiga = chave
-        chave = traduz_cesar(chave, todos_caracteres=True) % valores.TAMANHO_ASCII
-        return traduz_todos_caracteres(chave_antiga, chave, mensagem)
-    else:  # Se for modo encriptar, continuar nessa função para encriptar a mensagem.
-        chave = testa_chave_cesar(chave)
+    chave = retorna_chave_se_for_valida(chave)
     if chave:
         nova_mensagem = ''
         chave = int(chave) % valores.TAMANHO_ASCII
@@ -82,24 +78,38 @@ def executar_modo_varios_caracteres(chave, mensagem, modo_traducao=False):
         return 'Chave inválida !'
 
 
-def traduz_todos_caracteres(chave_antiga, chave_traduc, mensagem):
+def traduzir_modo_varios_caracteres(chave, mensagem):
     '''
     Função que traduzirá a cifra de césar no modo de todos os caracteres.
     Chave antiga recebe o valor da chave antiga (a chave que não passou pela transformação para a tradução).
     Chave traduc. recebe a chave adaptada para a tradução.
     Mensagem recebe o valor da mensagem encriptada, que precisa ser traduzida.
     '''
-    mensagem_traduzida = ''
-    for letra in mensagem:
-        letra_msg_atual = ord(letra)
-        letra_ASCII = letra_msg_atual + chave_traduc
-        verificar_volta = letra_msg_atual - int(chave_antiga)
-        if letra_msg_atual > valores.INICIO_VAZIO and verificar_volta - valores.TAMANHO_ESPAÇO_VAZIO < valores.INICIO_ASCII:
-            # Se a letra recebeu 2 vezes o número 34, é preciso voltar 34 na chave de tradução para não traduzir errado.
-            letra_ASCII -= valores.TAMANHO_ESPAÇO_VAZIO
-        if letra_msg_atual < valores.INICIO_VAZIO and verificar_volta > valores.INICIO_ASCII:
-            letra_ASCII += valores.TAMANHO_ESPAÇO_VAZIO    
-        if letra_ASCII > valores.FINAL_ASCII:  # O valor da letra atual a ser traduzida passou do valor final, hora de voltar para o ínicio.
-            letra_ASCII -= valores.VOLTAR_PARA_INICIO
-        mensagem_traduzida += chr(letra_ASCII)
-    return mensagem_traduzida
+    if not mensagem:
+        return 'Mensagem inválida !'
+    chave_traduc = adaptar_chave_para_traduçao_varios_caracteres(chave)
+    if chave:
+        mensagem_traduzida = ''
+        for letra in mensagem:
+            letra_msg_atual = ord(letra)
+            letra_ASCII = letra_msg_atual + chave_traduc
+            verificar_volta = letra_msg_atual - int(chave)
+            if letra_msg_atual > valores.INICIO_VAZIO and verificar_volta - valores.TAMANHO_ESPAÇO_VAZIO < valores.INICIO_ASCII:
+                # Se a letra recebeu 2 vezes o número 34, é preciso voltar 34 na chave de tradução para não traduzir errado.
+                letra_ASCII -= valores.TAMANHO_ESPAÇO_VAZIO
+            if letra_msg_atual < valores.INICIO_VAZIO and verificar_volta > valores.INICIO_ASCII:
+                # Se a letra não passou da "região não imprimível" é preciso somar mais 34 no caractere traduzido para sair certo.
+                letra_ASCII += valores.TAMANHO_ESPAÇO_VAZIO    
+            if letra_ASCII > valores.FINAL_ASCII:  # O valor da letra atual a ser traduzida passou do valor final, hora de voltar para o ínicio.
+                letra_ASCII -= valores.VOLTAR_PARA_INICIO
+            mensagem_traduzida += chr(letra_ASCII)
+        return mensagem_traduzida
+    else:
+        return 'Chave inválida !'
+
+
+def adaptar_chave_para_traduçao_varios_caracteres(chave):
+    chave = retorna_chave_se_for_valida(chave)
+    if chave:
+        chave = valores.TAMANHO_ASCII - (int(chave) % valores.TAMANHO_ASCII)
+    return chave
--- main.py
+++ main.py
@@ -23,28 +23,28 @@ def main():
     # Criar a interface principal do programa, utilizando o layout a cima.
     tela_principal = sg.Window('Criptografias: Tela principal',layout_principal)
     while True:  # Loop que verifica cada interação do usuário com o programa.
-        eventos, valores = tela_principal.read()
-        if eventos in ('6', None):
+        evento, valores = tela_principal.read()
+        if evento in ('6', None):
             # Fechar o programa
             tela_principal.close()
             break
-        if eventos == '1':
+        if evento == '1':
             # Esconder a tela principal e iniciar a interface "encriptar".
             tela_principal.Hide()
             menu_encriptar(tela_principal)
-        if eventos == '2':
+        if evento == '2':
             # Esconder a tela principal e iniciar a interface "traduzir".
             tela_principal.Hide()
             menu_traducao(tela_principal)
-        if eventos == '3':
+        if evento == '3':
             # Esconder a tela principal e iniciar a interface "documentação".
             tela_principal.Hide()
             menu_documentacao(tela_principal)
-        if eventos == '4':
+        if evento == '4':
             # Esconder a tela principal e iniciar a interface "testes automatizados".
             tela_principal.Hide()
             menu_testes(tela_principal)
-        if eventos == '5':
+        if evento == '5':
             # Esconder a tela principal e inicar a interface "força bruta".
             tela_principal.Hide()
             menu_forca_bruta(tela_principal)
@@ -92,41 +92,31 @@ def retorna_layout_padrao_encriptaçao(titulo, criptografia):
     return layout
 
 
-def menu_encriptar(tela_p):
-    # Menu de encriptação: contém alguns botões redirecionadores para menu das cifras.
+def menu_encriptar(tela_anterior):
     layout_encriptar = cria_layout_traduçao_e_encriptaçao('Pythografia: encriptar')
     tela_encriptar = sg.Window('Criptografias: encriptar', layout_encriptar)
     while True:
-        eventos, valores = tela_encriptar.read()
-        if eventos in ('retornar', None):
-            # Fechar tela atual e voltar para a tela principal.
-            tela_encriptar.Close()
-            tela_p.UnHide()
+        evento, valores = tela_encriptar.read()
+        if voltou_para_tela_anterior(evento, tela_anterior, tela_encriptar):
             break
-        tela_encriptar.Hide()
-        if eventos == 'Cifra de César':
+        if evento == 'Cifra de César':
             menu_cesar_encriptar(tela_encriptar)
-        elif eventos == 'Substituição simples':
+        elif evento == 'Substituição simples':
             menu_subst_simples_encriptar(tela_encriptar)
         else:
             menu_vigenere_encriptar(tela_encriptar)
-        
-        
 
-def menu_traducao(tela_p):
+
+def menu_traducao(tela_anterior):
     layout_traduzir = cria_layout_traduçao_e_encriptaçao('PythonGrafia: traduzir')
     tela_traduzir = sg.Window('Criptografias: traduzir', layout_traduzir)
     while True:
-        eventos, valores = tela_traduzir.read()
-        if eventos in ('2', None):
-            # Fechar tela atual e voltar para a tela principal.
-            tela_p.UnHide()
-            tela_traduzir.close()
+        evento, valores = tela_traduzir.read()
+        if voltou_para_tela_anterior(evento, tela_anterior, tela_traduzir):
             break
-        tela_traduzir.Hide()
-        if eventos == 'Cifra de César':
+        if evento == 'Cifra de César':
             menu_cesar_traduzir(tela_traduzir)
-        elif eventos == 'Substituição simples':
+        elif evento == 'Substituição simples':
             menu_subst_simples_traduzir(tela_traduzir)
         else:
             menu_vigenere_traduzir(tela_traduzir)
@@ -136,65 +126,65 @@ def menu_cesar_encriptar(tela_anterior):
     layout_cesar_encript = retorna_layout_padrao_encriptaçao('PythonGrafia: Cifra de César (encriptação)', 'Cifra de César')
     tela_cesar_encript = sg.Window('PythonGrafia: Cifra de César', layout_cesar_encript)
     while True:
-        eventos, valores = tela_cesar_encript.read()
-        if eventos in ('retornar', None):
-            print('oxi')
-            tela_anterior.UnHide()
-            tela_cesar_encript.Close()
+        evento, valores = tela_cesar_encript.read()
+        if voltou_para_tela_anterior(evento, tela_anterior, tela_cesar_encript):
             break
         if valores['apenas letras']:
-            print(Cifras.cifra_de_cesar.executar_modo_apenas_letras(valores['chave'], valores['mensagem']))
+            print(Cifras.cifra_de_cesar.encriptar_modo_apenas_letras(valores['chave'], valores['mensagem']))
         else:
-            print(Cifras.cifra_de_cesar.executar_modo_varios_caracteres(valores['chave'], valores['mensagem']))
+            print(Cifras.cifra_de_cesar.encriptar_modo_varios_caracteres(valores['chave'], valores['mensagem']))
 
 
 def menu_cesar_traduzir(tela_anterior):
     layout_cesar_traduc = retorna_layout_padrao_traduçao('PythonGrafia: Cifra de César (tradução)', 'Cifra de César')
     tela_cesar_traduc = sg.Window('PythonGrafia: Cifra de César', layout_cesar_traduc)
     while True:
-        eventos, opçao = tela_cesar_traduc.read()
+        evento, valores = tela_cesar_traduc.read()
+        if voltou_para_tela_anterior(evento, tela_anterior, tela_cesar_traduc):
+            break
+        if valores['apenas letras']:
+            print(Cifras.cifra_de_cesar.traduzir_modo_apenas_letras(valores['chave'], valores['mensagem']))
+        else:
+            print(Cifras.cifra_de_cesar.traduzir_modo_varios_caracteres(valores['chave'], valores['mensagem']))
 
 
 def menu_subst_simples_encriptar(tela_anterior):
     layout_subst_encript = retorna_layout_padrao_encriptaçao('PythonGrafia: Substituição simples (encriptação)', 'Substituição simples')
     tela_subst_encript = sg.Window('PythonGrafia: Substituição simples', layout_subst_encript)
     while True:
-        eventos, opçao = tela_subst_encript.read()
+        evento, valores = tela_subst_encript.read()
+        if voltou_para_tela_anterior(evento, tela_anterior, tela_subst_encript):
+            break
 
 
 def menu_subst_simples_traduzir(tela_anterior):
     layout_subst_traduc = retorna_layout_padrao_traduçao('PythonGrafia: Substituição simples (tradução)', 'Substituição simples')
     tela_subst_traduc = sg.Window('PythonGrafia: substituição simples', layout_subst_traduc)
     while True:
-        eventos, opçao = tela_subst_traduc.read()
+        evento, valores = tela_subst_traduc.read()
+        if voltou_para_tela_anterior(evento, tela_anterior, tela_subst_traduc):
+            break
 
 
 def menu_vigenere_encriptar(tela_anterior):
     layout_vigenere_encript = retorna_layout_padrao_encriptaçao('PythonGrafia: Cifra de Vigenère (encriptação)')
     tela_vigenere_encript = sg.Window('PythonGrafia: Cifra de Vigenère', layout_vigenere_encript)
     while True:
-        eventos, opçao = tela_vigenere_encript.read()
+        evento, valores = tela_vigenere_encript.read()
+        if voltou_para_tela_anterior(evento, tela_anterior, tela_vigenere_encript):
+            break
 
 
 def menu_vigenere_traduzir(tela_anterior):
     layout_vigenere_traduc = retorna_layout_padrao_traduçao('PythonGrafia: Cifra de Vigenère (tradução)')
     tela_vigenere_traduc = sg.Window('PythonGrafia: Cifra de Vigenère', layout_vigenere_traduc)
     while True:
-        eventos, opçao = tela_vigenere_traduc.read()
+        evento, valores = tela_vigenere_traduc.read()
+        if voltou_para_tela_anterior(evento, tela_anterior, tela_vigenere_traduzir):
+            break
 
 
-def emprimir_criptografia(dic_criptografia, modo=False):
-    '''
-    Função que imprimirá a nova mensagem (encriptada ou traduzida) na interface.
-    param dic_criptografia: contém algumas informações fornecidads pelo usuário,
-    que serão interpretadas pela função "executa_cifra".
-    param modo: Falso: encriptará | Verdadeiro: traduzirá.
-    '''
-    mensagem_nova = Cifras.executar.executa_cifra(dic_criptografia, modo_traducao=modo)
-    print(mensagem_nova)
-
-
-def menu_documentacao(tela_p):
+def menu_documentacao(tela_anterior):
     # Layou do menu de ajuda.
     layout_documentacao = [[sg.Text('        Menu de ajuda         ')],
                           [sg.Text('Na opção 1 do menu principal, escolha uma cifra, uma mensagem')],
@@ -205,57 +195,56 @@ def menu_documentacao(tela_p):
                           [sg.Text('das cifras disponiveis e suas funcionalidades.')],
                           [sg.Text('Acesse o site abaixo para ter mais informações:')],
                           [sg.Output()],
-                          [sg.Button('Retornar',key='retorno')]]
+                          [sg.Button('Retornar',key='retornar')]]
     tela_doc = sg.Window('Menu ajuda', layout_documentacao)
     ja_mostrou = False
     while True:
-        eventos, valores = tela_doc.read(timeout=1000)
+        evento, valores = tela_doc.read(timeout=1000)
         if not ja_mostrou:  # Mostrar o link apenas uma vez.
             print('https://github.com/GregorioFornetti/Programa-criptografia/wiki')
             ja_mostrou = True
-        if eventos in ('retorno', None):
-            # Usuário clicou em retornar ou fechar.
-            tela_p.UnHide()
-            tela_doc.close()
+        if voltou_para_tela_anterior(evento, tela_anterior, tela_doc):
             break
 
 
-def menu_testes(tela_p):
+def menu_testes(tela_anterior):
     layout_testes = [[sg.Text('Testes automatizados para verificar se o programa está funcionando corretamente.')],
                     [sg.Output(size=(120,40))],
                     [sg.Button('Retornar',key='retorno'), sg.Button('Testar',key='testar')]]
 
     tela_testes = sg.Window('Menu testes automatizados', layout_testes)
     while True:
-        eventos, valores = tela_testes.read()
-        if eventos in ('retorno', None):
-            # Usuário saiu do menu ou clicou em "retornar".
-            tela_p.UnHide()
-            tela_testes.close()
+        evento, valores = tela_testes.read()
+        if voltou_para_tela_anterior(evento, tela_anterior, tela_testes):
             break
-        if eventos in ('testar'):
+        if evento in ('testar'):
             # O usuário clicou em "testar".
             testes_simples.testar()
 
 
-def menu_forca_bruta(tela_p):
+def menu_forca_bruta(tela_anterior):
     layout_forca_bruta = [[sg.Text('             Força bruta: Cifra de César')],
                           [sg.Text('Escreva uma mensagem encriptada. Clique em testar e todas as chaves')],
                           [sg.Text('da cifra de césar serão testadas na força bruta.')],
                           [sg.Text('Mensagem encriptada:'), sg.Input(key='mensagem')],
                           [sg.Output(size=(100,35))],
-                          [sg.Button('Testar', key='testar'), sg.Button('Retornar', key='retorno')]]
+                          [sg.Button('Testar', key='testar'), sg.Button('Retornar', key='retornar')]]
     tela_forca_bruta = sg.Window('Menu força bruta', layout_forca_bruta)
     while True:
-        eventos, valores = tela_forca_bruta.read()
-        if eventos in ('retorno', None):
-            # Fechar a tela atual e abrir a tela principal.
-            tela_p.UnHide()
-            tela_forca_bruta.close()
+        evento, valores = tela_forca_bruta.read()
+        if voltou_para_tela_anterior(evento, tela_anterior, tela_forca_bruta):
             break
-        if eventos == 'testar':
+        if evento == 'testar':
             # Efetuar teste.
             forca_bruta.forca_bruta_cesar(valores['mensagem'])
+
+
+def voltou_para_tela_anterior(evento, tela_anterior, tela_atual):  # Volta para a tela anterior se usuário escolheu botão "retornar".
+    if evento in ('retornar', None):
+        tela_anterior.UnHide()
+        tela_atual.Close()
+        return True
+    return False
 
 
 main()
